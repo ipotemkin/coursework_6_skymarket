92b93fff1b04f22e7f55683856cb34f5b13939bb
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-03-28 17:26:22 +0300

    refactored to PhoneNumberField

--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -14,7 +14,8 @@ class User(AbstractBaseUser):
     email = models.EmailField(unique=True, verbose_name="Email address")
     first_name = models.CharField(max_length=64, verbose_name="Имя")
     last_name = models.CharField(max_length=64, verbose_name="Фамилия")
-    phone = models.CharField(max_length=128, verbose_name="Телефон для связи")
+    # phone = models.CharField(max_length=128, verbose_name="Телефон для связи")
+    phone = PhoneNumberField(null=True, blank=True, unique=False, verbose_name="Телефон для связи")
     role = models.CharField(max_length=5, choices=ROLES, default=UserRoles.USER, verbose_name="Роль")
     is_active = models.BooleanField(default=True)
 
