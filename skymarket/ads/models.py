bc31c4c60d1468f4900a97fce8679d426fbb95be
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-03-28 17:46:13 +0300

    removed unnecessary files

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,7 +1,5 @@
-from django.conf import settings
 from django.db import models
 
-from skymarket.settings import MEDIA_ROOT
 from users.models import User
 
 
@@ -9,16 +7,12 @@ class Ad(models.Model):
     image = models.ImageField(upload_to="images/", null=True, verbose_name="Фото")
     title = models.CharField(
         max_length=200,
-        # validators=[MinLengthValidator(10)],
         verbose_name="Название товара"
     )
     price = models.PositiveIntegerField(verbose_name="Цена товара")
     description = models.CharField(max_length=1000, verbose_name="Описание товара", null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
     created_at = models.DateTimeField(auto_now_add=True)
-
-    # is_published = models.BooleanField(default=False, verbose_name="Опубликован или нет")
-    # category = models.ForeignKey(Cat, on_delete=models.PROTECT, verbose_name="Категория")
 
     class Meta:
         verbose_name_plural = "Объявления"
@@ -46,6 +40,3 @@ class Comment(models.Model):
 
 ADO = Ad.objects  # noqa
 COMO = Comment.objects  # noqa
-# USERO = User.objects  # noqa
-# LOCO = Location.objects  # noqa
-# SELO = Selection.objects # noqa
